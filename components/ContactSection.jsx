8333ecec2553a918cd6dec13bbac05f109876eee
Andre Simmons <74680937+asimmons1129@users.noreply.github.com>
2023-07-06 12:22:16 -0500

    Add files via upload

--- components/ContactSection.jsx
+++ components/ContactSection.jsx
@@ -32,19 +32,19 @@ const Contact = () => {
           <form action="https://getform.io/f/f8c1fbbd-8138-49f6-90ba-f8eefd98f9d6" method="POST" className="ml-10 mr-10 md:px-4">
             <div className="grid md:grid-cols-2 gap-4 w-full py-2"> 
             <div className="flex flex-col">
-              <label className="text-purple-800 mb-2 ">Name</label>
+              <label className="font-bold text-purple-800 mb-2 ">Name</label>
               <input className="focus:border-teal-400 border-2 border-neutral-400 rounded-md flex shadow-lg mb-4" name="name" type="text" placeholder="Name"/>
             </div>
             <div className="flex flex-col">
-              <label className="text-purple-800 mb-2">Phone Number</label>
+              <label className="font-bold text-purple-800 mb-2">Phone Number</label>
               <input className="focus:border-teal-400 border-2 border-neutral-400 rounded-md flex shadow-lg mb-4" name="phone" type="tel" placeholder="Phone Number"/>
             </div>
             </div>
-            <label className="text-purple-800 mb-2">Email</label>
+            <label className="font-bold text-purple-800 mb-2">Email</label>
             <input className="focus:border-teal-400 border-2 border-neutral-400 rounded-md flex w-full shadow-lg mb-4" name="email" type="email" placeholder="Email"/>
-            <label className="text-purple-800 mb-2">Subject</label>
+            <label className="font-bold text-purple-800 mb-2">Subject</label>
             <input className="focus:border-teal-400 border-2 border-neutral-400 rounded-md flex w-full shadow-lg mb-4" name="subject" type="text" placeholder="Subject"/>
-            <label className="text-purple-800 mb-2">Message</label>
+            <label className="font-bold text-purple-800 mb-2">Message</label>
             <textarea className="focus:border-teal-400 border-2 border-neutral-400 rounded-md flex w-full shadow-lg" name="message" rows='10'></textarea>
             <button className="bg-purple-400 hover:bg-teal-700 rounded-md shadow-lg p-4 text-white mt-6">Let's Collaborate!</button>
           </form>
